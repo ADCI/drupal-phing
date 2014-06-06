2f7c8714ac50a3001dba7b229eed361f002323c3
Leonid.Kuznetsov <leonid.shmidt@gmail.com>
2014-06-06 17:01:52 +0700

    Drushrc file handling.

---
+++ build/files/drushrc.php
@@ -0,0 +1,20 @@
+<?php
+
+$options['structure-tables']['common'] = array(
+  'cache',
+  'cache_admin_menu',
+  'cache_block',
+  'cache_bootstrap',
+  'cache_field',
+  'cache_filter',
+  'cache_form',
+  'cache_image',
+  'cache_libraries',
+  'cache_menu',
+  'cache_page',
+  'cache_path',
+  'cache_rules',
+  'cache_update',
+  'cache_views',
+  'cache_views_data',
+);
